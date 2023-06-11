8ba3dc8d4bbf4ee4f3c6c4e02d6adf51942cfe59
aemustaf <aemustaf@localhost.localdomain>
2023-06-12 00:17:09 +0600

    added myFind.go

---
+++ day02/ex00/myFind.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	fFlag := flag.Bool("f", false, "find files")
+	dFlag := flag.Bool("d", false, "find directories")
+	slFlag := flag.Bool("sl", false, "find symlink")
+	extFlag := flag.String("ext", "", "filter by extension")
+	flag.Parse()
+
+	if *extFlag != "" && !*fFlag {
+		fmt.Println("Flag error. Use -f for using -ext")
+		os.Exit(1)
+	}
+	if !*fFlag && !*dFlag && !*slFlag {
+		*fFlag, *dFlag, *slFlag = true, true, true
+	}
+
+	f := func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if (info.Mode()&fs.ModeSymlink != 0) && *slFlag {
+			link, err := filepath.EvalSymlinks(path)
+			if err != nil {
+				link = "[broken]"
+			}
+			fmt.Printf("%s -> %s\n", path, link)
+		} else if info.IsDir() && *dFlag {
+			fmt.Println(path)
+		} else if *fFlag && info.Mode()>>9 == 0 {
+			if *extFlag != "" {
+				ext := "." + *extFlag
+				if filepath.Ext(path) == ext {
+					fmt.Println(path)
+				}
+			} else {
+				fmt.Println(path)
+			}
+		}
+		return nil
+	}
+	err := filepath.Walk(flag.Arg(0), f)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+}
